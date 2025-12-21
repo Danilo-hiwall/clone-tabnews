edeced0716c51512b999bf3de8f0fe4f9becccb4
Danilo Henrique <segueme13.99@gmail.com>
2025-12-21 02:56:40 +0000

    'database.js' and '.env'

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 17 + 1 as soma;");
+  console.log(result.rows);
   response.status(200).json({ nome: "danilo", idade: "36", casado: "Simmmm" });
 }
 
