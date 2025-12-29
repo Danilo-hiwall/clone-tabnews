ed5bfe959492fc8afd94f01a926a1448d7223c52
danilo <segueme13.99@gmail.com>
2025-12-28 21:52:18 -0500

    finish endpoint '/api/v1/status' endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,34 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 17 + 1 as soma;");
-  console.log(result.rows);
-  response.status(200).json({ nome: "danilo", idade: "36", casado: "Simmmm" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;")
+  const databaseVesionValue = databaseVersionResult.rows[0].server_version
+
+  const databaseMaxConnnectionsResult = await database.query("SHOW max_connections;")
+  const databaseMaxConnectionsValue = await databaseMaxConnnectionsResult.rows[0].max_connections
+
+  const dataName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dataName]
+  })
+
+  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVesionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+
+      }
+    }
+  });
 }
 
 export default status;
